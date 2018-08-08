2dd06c535a536598e9bc3091e9f67af176800159
Bryanv6 <vill.bryan72@gmail.com>
2018-08-08 19:30:20 -0400

    AddSpring Boot

---
+++ src/main/java/com/gamest/GamestApplication.java
@@ -0,0 +1,12 @@
+package com.gamest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestApplication.class, args);
+	}
+}
